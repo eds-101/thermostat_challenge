d9f7f3b6405cffe34c75574e88500b9ab071696e
Peter Allen <peter_james_allen@hotmail.com>
2021-03-09 15:46:27 +0000

    refactored maximum temperature

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -4,7 +4,7 @@ class Thermostat {
   constructor() {
   this.temperature = 20;
   this.MINIMUM_TEMP = 10;
-  this.MAXIMUM_TEMP = 25;
+  this.powerSavingOn();
   }
   getCurrentTemperature() {
     return this.temperature;
@@ -12,9 +12,10 @@ class Thermostat {
   up(number = 1) {
     if (this.temperature + number > this.MAXIMUM_TEMP) {
       this.temperature = this.MAXIMUM_TEMP;
-      throw 'Maximum temperature is 25 degrees';
+      throw `Maximum temperature is ${this.MAXIMUM_TEMP} degrees`;
+    } else {
+      this.temperature += number;
     }
-    this.temperature += number;
   }
 
   down(number = 1) {
@@ -27,7 +28,7 @@ class Thermostat {
   }
 
   powerSavingOn() {
-
+    this.MAXIMUM_TEMP = 25;
   }
 
 };
